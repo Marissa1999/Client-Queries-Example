b9ad2128d8c514477f66021354716a8309f0ccf3
Marissa Gonçalves <marissagoncalves13@gmail.com>
2021-01-05 11:45:16 -0500

    Initial Commit.

---
+++ SQLQuery1.sql
@@ -0,0 +1,16 @@
+﻿/*Display the Client Table Data*/
+SELECT * FROM Client;
+
+/*Display the Contact Table Data*/
+SELECT * FROM Contact;
+
+/*Output Records with the Name of the People and Their Contact Information*/
+SELECT Client.FirstName, Client.LastName, Contact.Type, Contact.Info 
+FROM Contact INNER JOIN Client 
+ON Client.Id = Contact.ClientId;
+
+/*Display the List of All People with No Contact Information*/
+SELECT Client.FirstName, Client.LastName 
+FROM Contact FULL OUTER JOIN Client 
+ON Client.Id = Contact.ClientId
+WHERE Contact.Type IS NULL AND Contact.Info IS NULL;
